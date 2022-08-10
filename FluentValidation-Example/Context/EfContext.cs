600e81e9d190be1938fbd7068ec1dc79fdd21e51
Fatih <fatihcihan666@gmail.com>
2022-08-10 16:42:42 +0300

    EfContext'te connection string tanimlandi ve db'ye ulasildi

---
+++ FluentValidation-Example/Context/EfContext.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation_Example.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentValidation_Example.Context
+{
+    public class EfContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Database=FluentDb;Uid=sa;Password=1");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
