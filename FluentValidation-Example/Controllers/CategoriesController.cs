7b6a87c346a02273cd7e36f595e064ea2082a761
Fatih <fatihcihan666@gmail.com>
2022-08-10 16:41:59 +0300

    Product ve category controller eklendi

--- FluentValidation-Example/Controllers/CategoriesController.cs
+++ FluentValidation-Example/Controllers/CategoriesController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentValidation_Example.Context;
+using FluentValidation_Example.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace FluentValidation_Example.Controllers
 {
@@ -7,5 +10,47 @@ namespace FluentValidation_Example.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        EfContext context = new EfContext();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int categoryId)
+        {
+            var category = context.Categories.FirstOrDefault(x => x.Id == categoryId);
+            return Ok(category);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int categoryId)
+        {
+            var category = context.Categories.FirstOrDefault(x => x.Id == categoryId);
+            context.Remove(category);
+            context.SaveChanges();
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return Ok(category);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(int categoryId, Category category)
+        {
+            var editCategory = context.Categories.FirstOrDefault(x => x.Id == categoryId);
+            editCategory.Name = category.Name;
+            editCategory.Description = category.Description;
+            context.Update(editCategory);
+            return Ok(editCategory);
+        }
     }
 }
--- FluentValidation-Example/Controllers/ProductsController.cs
+++ FluentValidation-Example/Controllers/ProductsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentValidation_Example.Context;
+using FluentValidation_Example.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FluentValidation_Example.Controllers
 {
@@ -7,5 +12,38 @@ namespace FluentValidation_Example.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        EfContext context = new EfContext();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var products = context.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int productId)
+        {
+            var product = context.Products.FirstOrDefault(x => x.Id == productId);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int productId)
+        {
+            var product = context.Products.FirstOrDefault(x => x.Id == productId);
+            context.Remove(product);
+            context.SaveChanges();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+            return Ok(product);
+        }
+
     }
 }
