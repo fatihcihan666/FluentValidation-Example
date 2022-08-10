bfabbd48310fc8106f8514087d26d3b4bd181ead
Fatih <fatihcihan666@gmail.com>
2022-08-10 16:45:22 +0300

    Code first icin gerekli migration atildi ve db'de tablolar olusturuldu

---
+++ FluentValidation-Example/Migrations/20220810104828_addDescriptionToCategoryColumn.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FluentValidation_Example.Migrations
+{
+    public partial class addDescriptionToCategoryColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
