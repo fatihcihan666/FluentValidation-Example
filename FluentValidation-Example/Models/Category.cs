2ada8cccfd3b6db29998dce14f6dbc561cd314de
Fatih <fatihcihan666@gmail.com>
2022-08-10 16:42:54 +0300

    Gerekli modeller tanimlandı

--- FluentValidation-Example/Models/Category.cs
+++ FluentValidation-Example/Models/Category.cs
@@ -1,6 +1,12 @@
-﻿namespace FluentValidation_Example.Models
+﻿using System.Collections.Generic;
+
+namespace FluentValidation_Example.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
